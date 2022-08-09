5a335b3a2baeb6c9a35db178670fa91df3415c38
Kamel Assaf <110683134+xikinho000@users.noreply.github.com>
2022-08-09 15:59:50 -0600

    Update WinAPI_5_WinSockets.c

--- WinAPI_5_WinSockets.c
+++ WinAPI_5_WinSockets.c
@@ -19,7 +19,7 @@ int main()
     // Server - Client; user decide
     char c;
     printf("s server \nk - client\n");
-    scanf("%c", c);
+    scanf("%c", &c);
     if(c=='c'){
         sa.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
         connect(s, &sa, sizeof(sa));
